dcbceaf7d3146b9961d14a5fc0ee93c2d78a72cd
Nick Rakoczy <nickrak.nr@gmail.com>
2017-06-03 00:00:14 -0600

    Stop allowing context push, doesn't work with functions

--- conductor-engine/src/main/java/urn/conductor/core/ContainerHandler.kt
+++ conductor-engine/src/main/java/urn/conductor/core/ContainerHandler.kt
@@ -2,13 +2,7 @@ package urn.conductor.core
 
 class ContainerHandler : urn.conductor.ElementHandler<urn.conductor.stdlib.xml.Container> {
 	override fun process(element: urn.conductor.stdlib.xml.Container, engine: urn.conductor.Engine, processChild: (Any) -> Unit) {
-		if (element.isIsolated) {
-			engine.nestContext {
-				element.any.forEach(processChild)
-			}
-		} else {
-			element.any.forEach(processChild)
-		}
+		element.any.forEach(processChild)
 	}
 
 	override fun getAttributes(element: urn.conductor.stdlib.xml.Container): Map<javax.xml.namespace.QName, String> = element.otherAttributes
